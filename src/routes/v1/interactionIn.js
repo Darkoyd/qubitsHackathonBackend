3e441e3f015d8ef67f45be288876f2824ea7f6f1
Juan Villamil <jd.villamil@uniandes.edu.co>
2020-07-27 19:28:33 -0500

    Interaction and Messege routes

---
+++ src/routes/v1/interactionIn.js
@@ -0,0 +1,61 @@
+const debug = require('debug')('backend:routes:interactionIn')
+const express = require('express')
+
+const router = express.Router()
+// eslint-disable-next-line no-undef
+const { InteractionIn, MessegeIn} = require(`${process.cwd()}/src/db`)
+const wrapper = require('express-debug-async-wrap')(debug)
+
+router.post('/:MessegeInId', wrapper( async (req, res) =>{
+	const messegeInId = req.params.MessegeInId
+	const messegeIn = await MessegeIn.findOne({ where: { id: messegeInId}})
+	if (messegeIn === 0) {
+		debug('MessegeIn didn\'t exist')
+	}
+	else{
+		//Si falla crear un nuevo Json
+		const interactionInJson = req.body
+		interactionInJson.MessegeIn = messegeInId
+		const interactionIn = await InteractionIn.create(interactionInJson)
+		res.send(interactionIn)
+	}
+	
+}))
+
+router.get('/', wrapper( async (req, res) =>{
+	const interactionIns = await InteractionIn.findAll()
+	res.send(interactionIns)
+}))
+
+router.get('/:InteractionInId', wrapper( async (req, res) =>{
+	const interactionIn = await InteractionIn.findOne({ where: { id: req.params.InteractionInId } })
+	if (interactionIn === 0) {
+		res.sendStatus(404)
+	}
+	else{
+		res.send(interactionIn)
+	}
+	
+}))
+
+router.put('/:InteractionInId',  wrapper( async (req, res) =>{
+	const interactionIn = await InteractionIn.update({where: {id: req.params.InteractionInId}}, req.body)
+	if (interactionIn === 0) {
+		res.sendStatus(404)
+	}
+	else{
+		res.send(interactionIn)
+	}
+}))
+
+router.delete('/:InteractionInId',  wrapper( async (req, res) =>{
+	const result = await InteractionIn.destroy({where: {id: req.params.InteractionInId}})
+	if (result === 0) {
+		res.sendStatus(404)
+	}
+	else{
+		res.sendStatus(200)
+	}
+}))
+
+module.exports = router
