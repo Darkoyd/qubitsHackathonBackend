1700b5682850a2bd71b603fd4b5dcbdcc252675b
Nicolás Londoño <nilocuru@hotmail.com>
2020-07-30 22:00:58 -0500

    Auth final hotfix (I think)

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ const { UserAuthToken } = require(`${process.cwd()}/src/db`)
 const debug = require('debug')('backend:authMiddleware')
 
 module.exports = async (req, res, next) => {
-	if (req.path.startsWith('/auth')) return next()
+	if (req.path.indexOf('/auth') === 0 || req.path.indexOf('/health') === 0) return next()
 	if (req.path === '/webhook') return next()
 	if (!req.get('x-auth-token')) return res.status(400).send('There is no auth token')
 	const token = await UserAuthToken.findOne({ where: { token: req.get('x-auth-token') } })
