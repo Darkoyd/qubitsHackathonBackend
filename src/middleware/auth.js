765a4330c8b6c122ddeedeee51b2e9144fdc777d
Nicolás Londoño <nilocuru@hotmail.com>
2020-07-31 00:24:27 -0500

    One route test

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,16 +3,12 @@ const { UserAuthToken } = require(`${process.cwd()}/src/db`)
 const debug = require('debug')('backend:authMiddleware')
 
 module.exports = async (req, res, next) => {
-	const route =  req.path.split('/')[1]
-	if (route === 'auth' || route === 'health' || route === 'webhook') {
-
-	} else {
-		if (!req.get('x-auth-token')) return res.status(400).send('There is no auth token')
-		const token = await UserAuthToken.findOne({ where: { token: req.get('x-auth-token') } })
-		if (token === null) {
-			debug('Unauthorized request')
-			return res.status(401).send('Unauthorized')
-		}
+	if (req.path.indexOf('/auth')) return next()
+	if (!req.get('x-auth-token')) return res.status(400).send('There is no auth token')
+	const token = await UserAuthToken.findOne({ where: { token: req.get('x-auth-token') } })
+	if (token === null) {
+		debug('Unauthorized request')
+		return res.status(401).send('Unauthorized')
 	}
 	next()
 }
