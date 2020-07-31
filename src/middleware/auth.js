480dfc8eb3dbf80d91db89fe3453cfb5e07ec437
Nicolás Londoño <nilocuru@hotmail.com>
2020-07-31 01:11:32 -0500

    Changed auth name

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,6 @@ const { UserAuthToken } = require(`${process.cwd()}/src/db`)
 const debug = require('debug')('backend:authMiddleware')
 
 module.exports = async (req, res, next) => {
-	if (req.path.indexOf('/auth') === 0) return await next()
 	if (!req.get('x-auth-token')) return res.status(400).send('There is no auth token')
 	const token = await UserAuthToken.findOne({ where: { token: req.get('x-auth-token') } })
 	if (token === null) {
