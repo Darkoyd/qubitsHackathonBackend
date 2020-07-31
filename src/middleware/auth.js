6ef72b3757f3d70f734f390955143f9d43ada585
Nicolás Londoño <nilocuru@hotmail.com>
2020-07-30 23:20:51 -0500

    Test

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,9 +3,6 @@ const { UserAuthToken } = require(`${process.cwd()}/src/db`)
 const debug = require('debug')('backend:authMiddleware')
 
 module.exports = async (req, res, next) => {
-	const route =  req.path.split('/')[1]
-	debug(route)
-	if (route === 'auth' || route === 'health' || route === 'webhook') return next()
 	if (!req.get('x-auth-token')) return res.status(400).send('There is no auth token')
 	const token = await UserAuthToken.findOne({ where: { token: req.get('x-auth-token') } })
 	if (token === null) {
